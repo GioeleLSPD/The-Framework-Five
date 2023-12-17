f8422fa6d2ade85cb1ae047043895629952b2746
GioeleLSPD <gioele.squizzato@student.h-farm.com>
2023-12-17 18:01:00 +0100

    Correct typo

--- backend/app/mymodules/mood.py
+++ backend/app/mymodules/mood.py
@@ -4,8 +4,8 @@ spotify_songs = pd.read_csv('/app/app/spotify_songs.csv')
 
 
 def find_songs_for_party():
-    """"Return a list of dictionaries with 50 songs
-    with values inherent with party playlist"""
+    """Return a list of dictionaries with 50 songs
+       with values inherent with party playlist"""
     features = ((spotify_songs['danceability'].between(0.6, 1.0)) &
                 (spotify_songs['energy'].between(0.6, 1.0)) &
                 (spotify_songs['loudness'].between(-60, 0)) &
@@ -18,8 +18,8 @@ def find_songs_for_party():
 
 
 def find_songs_for_chill():
-    """"Return a list of dictionaries with 50 songs
-    with values inherent with chill playlist"""
+    """Return a list of dictionaries with 50 songs
+       with values inherent with chill playlist"""
     features = ((spotify_songs['danceability'] < 0.6) &
                 (spotify_songs['energy'] < 0.5) &
                 (spotify_songs['speechiness'] < 0.5) &
@@ -30,8 +30,8 @@ def find_songs_for_chill():
 
 
 def find_songs_for_workout():
-    """"Return a list of dictionaries with 50 songs
-    with values inherent with workout playlist"""
+    """Return a list of dictionaries with 50 songs
+       with values inherent with workout playlist"""
     features = ((spotify_songs['energy'] > 0.6) &
                 (spotify_songs['tempo'] > 120) &
                 (spotify_songs['danceability'] > 0.5) &
@@ -42,8 +42,8 @@ def find_songs_for_workout():
 
 
 def find_songs_for_passion():
-    """"Return a list of dictionaries with 50 songs
-    with values inherent with passion playlist"""
+    """Return a list of dictionaries with 50 songs
+       with values inherent with passion playlist"""
     features = ((spotify_songs['danceability'].between(0.3, 0.7)) &
                 (spotify_songs['energy'].between(0.4, 0.8)) &
                 (spotify_songs['loudness'].between(-60, 0)) &
@@ -56,8 +56,8 @@ def find_songs_for_passion():
 
 
 def discover_random_song():
-    """"Return a list of dictionaries containing
-    a random song of the csv """
+    """Return a list of dictionaries containing
+       a random song of the csv"""
     random_song = spotify_songs.sample(
         1)[['track_name', 'track_artist']].to_dict(orient='records')
     return random_song
