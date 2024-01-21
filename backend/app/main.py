db0034516271b37f363cb7c98390aa1be2ac0f12
U20-LSPD <umberto.bolzoni@student.h-farm.com>
2024-01-21 23:59:43 +0100

    fixed pep8

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,8 @@ from .mymodules.mood import (find_songs_for_party, find_songs_for_chill,
                              discover_random_song)
 
 from .mymodules.info import (count_songs, artist_songs,
-                             get_song_count_by_genre, genre_popularity, get_songs_by_artist)
+                             get_song_count_by_genre, genre_popularity,
+                             get_songs_by_artist)
 
 app = FastAPI()
 
@@ -35,7 +36,8 @@ def get_songs_by_mood(mood_name: str):
     """
     Retrieve a list of songs matching a specified mood.
 
-    This endpoint is designed to return a list of songs associated with a specific mood.
+    This endpoint is designed to return a list of songs
+    associated with a specific mood.
     The mood is specified in the 'mood_name' path parameter.
 
     Args:
@@ -62,7 +64,8 @@ def get_info(info_g: str):
     """
     Retrieve specific music-related information based on the given category.
 
-    This endpoint is designed to retrieve and return music-related information based on the specified category.
+    This endpoint is designed to retrieve and return music-related
+    information based on the specified category.
     The category is provided in the 'info_g' path parameter.
 
     Args:
@@ -76,20 +79,24 @@ def get_info(info_g: str):
     else:
         raise HTTPException(status_code=404, detail="Information not found")
 
+
 @app.get('/songs/{artist_name}')
 def artist_song_list(artist_name: str):
     """
     Retrieve the list of songs for a given artist.
 
-    This endpoint is designed to retrieve and return a list of songs by a specific artist.
+    This endpoint is designed to retrieve and return a list
+    of songs by a specific artist.
     The artist name is provided in the 'artist_name' path parameter.
 
     Args:
-    artist_name (str): The name of the artist for whom the song list is to be retrieved.
+    artist_name (str): The name of the artist for whom the song
+    list is to be retrieved.
 
     Returns:
     list: A list of dictionaries representing songs by the specified artist.
-          Each dictionary contains information about a song, including 'track_name' and 'track_artist'."""
+          Each dictionary contains information about a song,
+          including 'track_name' and 'track_artist'."""
     songs_list = get_songs_by_artist(artist_name)
     if songs_list:
         return songs_list
